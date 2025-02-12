2b3894f1a6e0c3193b0cc88e9a1f46a7999eae84
Linkinshura <Ian.linkinshura@gmail.com>
2025-02-12 12:15:13 -0300

    Funciones 1

---
+++ Funciones/1.c
@@ -0,0 +1,13 @@
+/*
+ * Escribir una función en C, saludar() que imprima un saludo en pantalla.
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hola");
+
+	return 0;
+}
+
